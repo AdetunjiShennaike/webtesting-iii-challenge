14d8ae145e867436ef4290e4a9be9c0be735c03a
Adetunji <ashennaike@gmail.com>
2019-08-19 17:34:10 -0400

    never pushed it the first time haha

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,36 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent } from 'react-testing-library'
+import 'react-testing-library/cleanup-after-each'
+
+import Display from './Display'
+
+describe('display component', () => {
+  it('displays open and green', () => {
+    Display.defaultProps.closed = false
+    const led = Display.closedClass
+    console.log(led)
+  
+    const { getByText } = render(<Display />)
+
+    getByText(/Open/)
+  })
+  it('displays closed and red', () => {
+    Display.defaultProps.closed = true
+    const { getByText } = render(<Display />)
+
+    getByText(/Closed/)
+  })
+  it('displays locked and red', () => {
+    Display.defaultProps.locked = true
+    const { getByText } = render(<Display />)
+
+    getByText(/Locked/)
+  })
+  it('displays unlocked and green', () => {
+    Display.defaultProps.locked = false
+    const { getByText } = render(<Display />)
+
+    getByText(/Unlocked/)
+  })
+})
