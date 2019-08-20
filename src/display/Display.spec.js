8fef04e570b22c1030c8d4e8037d45130ce94a60
Adetunji <ashennaike@gmail.com>
2019-08-19 21:41:35 -0400

    added the dashboard with a snapshot and completed the control spec with comments

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,19 +1,22 @@
 // Test away!
 import React from 'react';
-import { render, fireEvent } from 'react-testing-library'
-import 'react-testing-library/cleanup-after-each'
+import { render, fireEvent } from '@testing-library/react'
+// import '@testing-library/react/cleanup-after-each' <-- deprecated 
 
 import Display from './Display'
 
 describe('display component', () => {
+  //check that things will be displayed properly based on the given boolean values
   it('displays open and green', () => {
+    //pass a value to the variable that controls if the door is open or closed
     Display.defaultProps.closed = false
-    const led = Display.closedClass
-    console.log(led)
-  
+    //grab a render of the page
     const { getByText } = render(<Display />)
-
-    getByText(/Open/)
+    //check the status of the text on the page
+    const Open = getByText(/Open/)
+    Open
+    //grab the color from the page and check its status
+    expect(Open.className).toMatch('led green-led')
   })
   it('displays closed and red', () => {
     Display.defaultProps.closed = true
