cf9aecf265c470b236ef6719cefd5c4975e5bf3d
Adetunji <ashennaike@gmail.com>
2019-08-19 21:46:33 -0400

    added comments to the display file and everything is done

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -13,27 +13,45 @@ describe('display component', () => {
     //grab a render of the page
     const { getByText } = render(<Display />)
     //check the status of the text on the page
-    const Open = getByText(/Open/)
-    Open
+    const open = getByText(/open/i)
+    open
     //grab the color from the page and check its status
-    expect(Open.className).toMatch('led green-led')
+    expect(open.className).toMatch('led green-led')
   })
+
   it('displays closed and red', () => {
+    //pass a true so that the door is closed
     Display.defaultProps.closed = true
+    
     const { getByText } = render(<Display />)
-
-    getByText(/Closed/)
+    //check that the page is rendered with closed
+    const closed =  getByText(/closed/i)
+    closed
+    //grab the color and check that its red
+    expect(closed.className).toMatch('led red-led')
   })
+
   it('displays locked and red', () => {
+    //set the status of the lock to true
     Display.defaultProps.locked = true
-    const { getByText } = render(<Display />)
 
-    getByText(/Locked/)
+    const { getByText } = render(<Display />)
+    //check if the door is locked
+    const locked = getByText(/Locked/i)
+    locked 
+    //check if the color is red
+    expect(locked.className).toMatch('led red-led')
   })
-  it('displays unlocked and green', () => {
-    Display.defaultProps.locked = false
-    const { getByText } = render(<Display />)
 
-    getByText(/Unlocked/)
+  it('displays unlocked and green', () => {
+    //set false to the lock value
+    Display.defaultProps.locked = false
+
+    const { getByText } = render(<Display />)
+    //check if the door is unlocked
+    const unlocked = getByText(/Unlocked/i)
+    unlocked
+    //check if the status is green
+    expect(unlocked.className).toMatch('led green-led')
   })
 })
